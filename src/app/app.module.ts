645517a10d678216205462f2395e2f8165518560
Sekenx <seken@gmx.com>
2019-04-17 13:20:12 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SharedComponent } from './shared/shared.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
+
+const appRoutes: Routes = [
+];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
